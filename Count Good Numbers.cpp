55639ba98df7e59f3189d2a540b654e3714b16b3
kedarnathpc <kedarnathpc2002@gmail.com>
2024-03-06 18:56:01 +0530

    solved

---
+++ Count Good Numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mod = 1e9+7;
+    long long power(long long x, long long y){
+        if(y == 0)
+            return 1;
+        
+        long long ans = power(x, y/2);
+
+        ans = (ans * ans) % mod;
+
+        if(y % 2)
+            ans = (ans * x) % mod;
+        
+        return ans;
+    }
+
+    int countGoodNumbers(long long n) {
+        long long odd = n/2, even = n/2 + n%2;
+        return (power(5, even) * power(4, odd))%mod;
+    }
+};
