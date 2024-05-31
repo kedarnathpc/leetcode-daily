24b01406072fddb7e50f73d48f3ec33368dd866c
kedarnathpc <kedarnathpc2002@gmail.com>
2024-05-31 10:43:41 +0530

    daily solved

---
+++ Single Number III.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        vector<int> res;
+
+        for (auto &i : nums)
+            mp[i]++;
+
+        for (auto &i : mp)
+            if (i.second == 1)
+                res.push_back(i.first);
+
+        return res;
+    }
+};
