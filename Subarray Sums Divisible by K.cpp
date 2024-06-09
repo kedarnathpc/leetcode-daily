90cca3b5d107bcac5b3a39719b83e3c16b9be15b
kedarnathpc <kedarnathpc2002@gmail.com>
2024-06-09 07:53:59 +0530

    daily solved

---
+++ Subarray Sums Divisible by K.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1; 
+        int count = 0, sum = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            int remainder = sum % k;
+
+            if (remainder < 0) {
+                remainder += k;
+            }
+
+            if (mp.find(remainder) != mp.end()) {
+                count += mp[remainder];
+            }
+
+            mp[remainder]++;
+        }
+
+        return count;
+    }
+};
